eec3c316e8cc5048bbba5b6c228fa1a744f876c0
Kleina13 <ragnarhelgi22@gmail.com>
2020-05-05 04:50:17 +0000

    aaaaaaaaaaa

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-fetch('data.json')
-    .then(res => res.json())
-    .then(data => { console.log(data) })
-    .catch(err => console.error(err));
+"use strict";
 
-for (var key in data) {
-    block_to_insert = document.createElement('div');
-    block_to_insert.style = ";";
-    block_to_insert.innerHTML = data
-    document.body.firstChild.insertAdjacentElement('beforebegin', block_to_insert);
+const div = document.getElementById("content");
+
+fetch('https://apis.is/earthquake/is')  
+.then( (response) => {
+    if (response.status !== 200) { // response handling
+        console.log('Looks like there was a problem. Status Code: ' + response.status);
+        return;
+    }
+
+    response.json().then( (data) => {
+    console.log(data);  // logs all the data into the console
+    
+    let the_div  = document.createElement('div'),
+        the_img  = document.createElement('img'),
+        the_text = document.createElement('h2');
+
+    for (var i = 0; i < data.results.length; i++) {
+        the_div.classList = "item"; // class 
+        the_img.src = "dog.png" // image source
+        the_text.innerHTML = data.results[i].humanReadableLocation; // name/text
+        the_div.appendChild(the_img);
+        the_div.appendChild(the_text);
+        div.appendChild(the_div);
+    }
+    
+    });
 }
+)
+.catch( (err) => { // error handling
+    console.log('Fetch Error :-S', err);
+})
+    
